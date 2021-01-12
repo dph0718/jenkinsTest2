5f8d80cdefa70edf301eb07366c151763b219d64
David Huffman <dph0718@gmail.com>
2021-01-11 20:56:10 -0700

    Adds test

--- test.test.js
+++ test.test.js
@@ -1,10 +1,15 @@
-import { Selector } from 'testcafe';
+import { Selector, t } from 'testcafe';
 
-fixture `Going to Google`
-    .page `http://www.google.com`;
+fixture`Going to Google`
+    .page`http://www.google.com`;
 
 test('My first test', async t => {
     // Test code
 
     console.log("Happy Loggin Test")
+
 });
+
+test("My second test.", async t => {
+    await t.expect(false).ok()
+})
